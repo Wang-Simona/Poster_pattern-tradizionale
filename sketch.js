9dd10c6e559e4927829e235b841bd7915ee70aae
Wang-Simona <wyingxuan9@gmail.com>
2025-05-23 14:46:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,61 @@
-let svgPaths1 = [
-  'svgs/2.svg', 'svgs/4.svg', 'svgs/5.svg', 'svgs/6.svg', 'svgs/7.svg',
-  'svgs/8.svg', 'svgs/9.svg', 'svgs/10.svg', 'svgs/14.svg', 'svgs/15.svg'
-];
-let svgPaths2 = [
-  'svgs/1.svg', 'svgs/3.svg', 'svgs/11.svg', 'svgs/12.svg', 'svgs/13.svg'
-];
-
 let svgs1 = [];
 let svgs2 = [];
+let totalToLoad = 0;
+let totalLoaded = 0;
+
 let G;
+let buff;
 let gridDivsX = 5; // gridDivsX 和 gridDivsY 是网格的横纵分割数
 let gridDivsY = 7;
 let gSX, gSY, pad = 5; //只把 pad 初始化为 10
 
 function preload() {
-  // 加载 1x1 的 SVG 图块
-  svgPaths1.forEach(path => {
-    fetch(path).then(res => res.text()).then(txt => svgs1.push(txt));
+  let svgs1Paths = [
+    'svgs/2.svg',
+    'svgs/4.svg',
+    'svgs/5.svg',
+    'svgs/6.svg',
+    'svgs/7.svg',
+    'svgs/8.svg',
+    'svgs/9.svg',
+    'svgs/10.svg',
+    'svgs/14.svg',
+    'svgs/15.svg',
+  ];
+
+  let svgs2Paths = [
+    'svgs/1.svg',
+    'svgs/3.svg',
+    'svgs/11.svg',
+    'svgs/12.svg',
+    'svgs/13.svg',
+  ];
+
+  totalToLoad = svgs1Paths.length + svgs2Paths.length;
+
+  svgs1Paths.forEach(path => {
+    loadImage(path, img => {
+      svgs1.push(img);
+      totalLoaded++;
+    });
   });
-  // 加载 2x2 的 SVG 图块
-  svgPaths2.forEach(path => {
-    fetch(path).then(res => res.text()).then(txt => svgs2.push(txt));
+
+  svgs2Paths.forEach(path => {
+    loadImage(path, img => {
+      svgs2.push(img);
+      totalLoaded++;
+    });
   });
 }
 
 function setup() {
-  createCanvas(595, 842); // createCanvas(由于我们自己生成 SVG 不需要 SVG 模式)
+  //createCanvas(2480, 3508, SVG);  // 第三个参数指定使用 SVG 模式
+  createCanvas(595, 842, SVG);
+  buff = createGraphics(width, height, SVG); // 注意：如果你打算导出 SVG，就需要这个缓冲区也用 SVG 渲染器
+
+  buff.background(255);
+  fill(150);
+  stroke(150);
 
   // 计算单格尺寸
   gSX = (width - 2 * pad) / gridDivsX;
@@ -33,44 +63,118 @@ function setup() {
 
   // 创建网格处理器
   G = new makeGrid(gridDivsX, gridDivsY);
+  // 创建布尔类型的网格（可用于标记格子是否已占用）
   G.setupGrid();
+  // 用矩形填充这个网格
+  // 每个矩形的信息保存在名为 rectInfo 的数组中
   G.populateGrid();
-
-  noLoop();
 }
 
 function draw() {
-  background(255);
-  fill(150);
-  stroke(150);
-
-  // 阴线编排 SVG 内容
-  let svgHeader = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<svg xmlns="http://www.w3.org/2000/svg" width="595" height="842" viewBox="0 0 595 842">`;
-  let svgContent = '';
-
-  for (let n = 0; n < G.rectInfo.length; n++) {
-    let R = G.rectInfo[n];
-    let randSvg;
-    if (R.dimX > 1) {
-      randSvg = random(svgs2);
-    } else {
-      randSvg = random(svgs1);
-    }
-
-    let x = R.posX * gSX + pad;
-    let y = R.posY * gSY + pad;
-    let wid = R.dimX * gSX;
-    let hei = R.dimY * gSY;
-
-    // 将 SVG 图块展开到指定位置和尺寸
-    let wrapped = `<g transform="translate(${x},${y}) scale(${wid/100},${hei/100})">` +
-                  extractSvgContent(randSvg) + `</g>`;
-    svgContent += wrapped;
+  // 防止资源未加载时出错
+  if (totalLoaded < totalToLoad || !G) {
+    background(255);
+    fill(100);
+    textSize(32);
+    text('Loading...', 50, 50);
+    return;
   }
 
-  svgContent = svgHeader + svgContent + '</svg>';
+  background(255);
 
-  // 展示一个按钮用于保存
-  let button = createButton('Download SVG');
-  button.position(10, 10);
-  
+  for (let n = 0; n < G.rectInfo.length; n++) {
+    let R = G.rectInfo[n]; //G一个数组，存储所有生成的矩形块的位置与尺寸
+                            //R是要绘制的矩形
+    // 如果这个矩形跨越多个格子（也就是尺寸大于 1x1）
+    // 我们就从 2x2 的 SVG 图块中随机选一个
+    // 否则就从 1x1 的图块中选一个
+    let randSvg;
+    if (R.dimX > 1) {
+      randSvg = random(svgs2); //2x2格子
+    } else {
+      randSvg = random(svgs1); //1x1格子
+    }
+
+    // 计算这个图块的绘制位置和尺寸
+    var x = R.posX * gSX + pad;   // X 位置：网格坐标 * 单格宽度(gSX) + 内边距(pad)
+    var y = R.posY * gSY + pad;   // Y 位置：网格坐标 * 单格高度(gSY) + 内边距(pad)
+    var wid = R.dimX * gSX;       // 宽度：跨多少格 * 单格宽度
+    var hei = R.dimY * gSY;       // 高度
+
+    // 在图形缓冲区上绘制这个 SVG 图块
+    buff.image(randSvg, x, y, wid, hei);
+  }
+
+  // 显示到主画布
+  image(buff, 0, 0);
+
+  noLoop(); // 只运行一次
+}
+
+// makeGrid 函数
+function makeGrid(xCount, yCount) {
+  this.gridW = xCount; // 网格宽度（列数）
+  this.gridH = yCount; // 网格高度（行数）
+  this.boolGrid = [];  // 布尔网格，记录哪些格子已经被使用
+  this.rectInfo = [];  // 存储已放置图块的信息（位置+尺寸）
+
+  // 初始化布尔网格（全部为 true 表示可用）
+  this.setupGrid = function () {
+    for (let x = 0; x < this.gridW; x++) {
+      let col = [];
+      for (let y = 0; y < this.gridH; y++) {
+        col.push(true);
+      }
+      this.boolGrid.push(col);
+    }
+  };
+
+  // 使用不重叠策略填充 1x1 或 2x2 的块
+  this.populateGrid = function () {
+    for (let x = 0; x < this.gridW; x++) {
+      for (let y = 0; y < this.gridH; y++) {
+
+        // 如果这个格子已被占用，跳过
+        if (!this.boolGrid[x][y]) continue;
+
+        let dimX = 1;
+        let dimY = 1;
+
+        // 50% 概率尝试放置 2x2
+        if (random() > 0.5) {
+          if (
+            x < this.gridW - 1 &&
+            y < this.gridH - 1 &&
+            this.boolGrid[x + 1][y] &&
+            this.boolGrid[x][y + 1] &&
+            this.boolGrid[x + 1][y + 1]
+          ) {
+            dimX = 2;
+            dimY = 2;
+          }
+        }
+
+        // 标记占用
+        for (let i = x; i < x + dimX; i++) {
+          for (let j = y; j < y + dimY; j++) {
+            this.boolGrid[i][j] = false;
+          }
+        }
+
+        // 保存矩形信息
+        this.rectInfo.push({
+          posX: x,
+          posY: y,
+          dimX: dimX,
+          dimY: dimY,
+        });
+      }
+    }
+  };
+}
+
+function keyPressed() {
+  if (key === 's') {
+    save(buff, 'poster.svg');
+  }
+}
